6f50eded9fb11946148c7be2e99e96049d2cc8fe
Nuziko <161970709+Nuziko@users.noreply.github.com>
2026-03-21 18:26:19 +0000

    Agent architechture setup

---
+++ agent/state.py
@@ -0,0 +1,14 @@
+from langgraph.graph import MessagesState
+from typing import Optional,Literal
+
+class MedicalState(MessagesState):
+    query :str=""
+    summary :Optional[str] = None
+    safety_status :Literal['safe','unsafe'] = 'safe'
+    answer:str=""
+    urls:list[str]=[]
+
+
+
+
+
